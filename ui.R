f49125104860033308257722c03d2d3ec7ca9e90
Nick Cassidy <nicholas.cassidy@improvementservice.org.uk>
2017-07-20 16:42:40 +0100

    First commit

---
+++ ui.R
@@ -0,0 +1,18 @@
+library(shiny)
+
+shinyUI(navbarPage(id = "pageList",
+
+  title = "Test LGBF Tool", 
+#Can change the theme - see shinyTheme package  
+  theme = shinytheme("simplex"),
+  tabPanel
+  sidebarLayout(
+    sidebarPanel(
+
+    ),
+
+    mainPanel(
+  
+    )
+  )
+))
