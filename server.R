252241747d96b6c6854f2787665ca8677612049b
Cara Connachan <cara.connachan@improvementservice.org.uk>
2017-08-10 10:56:59 +0100

    adjusted graph features

--- server.R
+++ server.R
@@ -7,9 +7,12 @@ shinyServer(function(input, output) {
   
     output$plot1 <- renderPlot({
     colnames(bnch_data)[1] <- "Local_Authority"
+    bnch_data <- filter(bnch_data, Local_Authority %in% input$LA)
     ggplot(bnch_data[bnch_data$Indicator2 == input$indicator2,], 
            aes(x = Local_Authority, y = Value, fill = Time))+
-      geom_bar(position = "dodge", stat = "identity")
+      geom_bar(position = "dodge", stat = "identity")+
+      theme_bw()+
+      guides(fill = FALSE)
             
   })
  
