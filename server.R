06f351f3b7383b55e1fd22ac6eff9ad25cd558f4
Cara Connachan <cara.connachan@improvementservice.org.uk>
2017-08-10 09:46:43 +0100

    created basic graph

--- server.R
+++ server.R
@@ -1,11 +1,12 @@
 shinyServer(function(input, output) {
   
-  output$plot1 <- renderPlot(
+  output$plot1 <- renderPlot({
+    colnames(bnch_data)[1] <- "Local_Authority"
     ggplot(bnch_data[bnch_data$Indicator2 == "CHN1",], 
-           aes(x = 'Local Authority', y = Value, fill = Time))+
+           aes(x = Local_Authority, y = Value, fill = Time))+
       geom_bar(position = "dodge", stat = "identity")
             
-  )
+  })
  
 
   })
