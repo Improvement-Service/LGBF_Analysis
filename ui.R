fe3b46fe55da27c4655efa88ffac30085e0e6082
Nicholas Cassidy <nicholas.cassidy@improvementservice.org.uk>
2017-08-11 10:03:07 +0100

    Add scroll effect to sidebar

--- ui.R
+++ ui.R
@@ -1,10 +1,10 @@
 shinyUI(navbarPage(id = "pageList",
-
+                   
   title = "Test LGBF Tool", 
 #Can change the theme - see shinyTheme package  
   theme = shinytheme("simplex"),
   tabPanel("Something",
-    sidebarPanel(
+    sidebarPanel(id = "sidPnl", style = "height:90vh;overflow-y:auto;",
       selectInput("category", "Select Indicator Category", unique(excl_Scotland$Domain),selected = "Children's Services"),
       uiOutput("indicator"),
       checkboxGroupInput("LA", "Select Local Authority", unique(excl_Scotland$`Local Authority`), selected = unique(excl_Scotland$'Local Authority')),
