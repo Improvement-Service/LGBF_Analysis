654269604c19b52d174022a832c82b10a7dcbbf3
Nick Cassidy <nicholas.cassidy@improvementservice.org.uk>
2017-08-14 15:30:31 +0100

    Remove NA Values

--- global.r
+++ global.r
@@ -5,7 +5,9 @@ library(shinythemes)
 
 
 bnch_data <- read_csv("bnch_dta_macrosttest.csv")
-
+#get rid of NA values
+bnch_data <- bnch_data[complete.cases(bnch_data$Value),]
+#Tidy names
 bnch_data$Time<- gsub("^14-15", "2014-15", bnch_data$Time)
 bnch_data$Time<- gsub("^15-16", "2015-16", bnch_data$Time)
 bnch_data$Time <- gsub("/", "-", bnch_data$Time)
