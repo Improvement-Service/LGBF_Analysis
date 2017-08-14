15b9698f2fdffce5a38ce202e536be20b61059c3
Nick Cassidy <nicholas.cassidy@improvementservice.org.uk>
2017-08-14 14:27:16 +0100

    Add new dataset

--- global.r
+++ global.r
@@ -5,7 +5,12 @@ library(shinythemes)
 
 
 bnch_data <- read_csv("bnch_dta_macrosttest.csv")
+
+bnch_data$Time<- gsub("^14-15", "2014-15", bnch_data$Time)
+bnch_data$Time<- gsub("^15-16", "2015-16", bnch_data$Time)
+bnch_data$Time <- gsub("/", "-", bnch_data$Time)
+bnch_data <- filter(bnch_data, !Time %in% c("1974", "2012", "2013"))
+##Tidy up the names for 
+#The Gross Cost of \"Children Looked After\" in a Community Setting per Child per Week adjusted for inflation
+bnch_data$Title <- gsub('\"', "", bnch_data$Title)
 excl_Scotland <- filter(bnch_data, `Local Authority` != "Scotland")
-excl_Scotland$Time<- gsub("^14-15", "2014-15", excl_Scotland$Time)
-excl_Scotland$Time<- gsub("^15-16", "2015-16", excl_Scotland$Time)
-excl_Scotland$Time <- gsub("/", "-", excl_Scotland$Time)
