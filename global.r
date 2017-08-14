c26fd5c9104d8f2e3d0d9930cbfce4ea3872c162
Cara Connachan <cara.connachan@improvementservice.org.uk>
2017-08-14 12:38:43 +0100

    edited Time column so that all years are in the same format

--- global.r
+++ global.r
@@ -5,3 +5,6 @@ library(shinythemes)
 
 bnch_data <- read_csv("bnch_dta_macrosttest.csv")
 excl_Scotland <- filter(bnch_data, `Local Authority` != "Scotland")
+excl_Scotland$Time<- gsub("^14-15", "2014-15", excl_Scotland$Time)
+excl_Scotland$Time<- gsub("^15-16", "2015-16", excl_Scotland$Time)
+excl_Scotland$Time <- gsub("/", "-", excl_Scotland$Time)
